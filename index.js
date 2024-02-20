6deec7f60a4058e3debbe6cddf8830bd7a587a65
harshnugget <76251920+harshnugget@users.noreply.github.com>
2024-02-20 09:16:44 +0000

    Create basic calculator functionality
    Functions:
        - Add
        - Subtract
        - Multiply
        - Divide

---
+++ index.js
@@ -0,0 +1,19 @@
+function add(addend1, addend2) {
+    const sum = addend1 + addend2
+    return sum;
+}
+
+function subtract(minuend, subtrahend) {
+    const difference = minuend - subtrahend
+    return difference;
+}
+
+function multiply(multiplicand, multiplier) {
+    const product = multiplicand * multiplier
+    return product;
+}
+
+function divide(dividend, divisor) {
+    const quotient = dividend / divisor;
+    return quotient;
+}
