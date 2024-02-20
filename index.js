47b12b6d485c1a54cee8c6d78b419987acaeb92f
harshnugget <76251920+harshnugget@users.noreply.github.com>
2024-02-20 09:29:34 +0000

    Function to execute any operation
    Created a calculate function that performs an operation depending on the operator provided
        - Catches division by 0

--- index.js
+++ index.js
@@ -17,3 +17,26 @@ function divide(dividend, divisor) {
     const quotient = dividend / divisor;
     return quotient;
 }
+
+let firstNumber;
+let operator;
+let secondNumber;
+
+function calculate(firstNumber, operator, secondNumber) {
+    switch (operator) {
+        case "+":
+            return add(firstNumber, secondNumber);
+        case "-":
+            return subtract(firstNumber, secondNumber);
+        case "*":
+            return multiply(firstNumber, secondNumber);
+        case "/":
+            // Handle divide by 0
+            if (secondNumber === 0) {
+                return console.log("Cannot divide by 0");
+            }
+            return divide(firstNumber, secondNumber);
+        case "":
+            return console.log("No operation");
+    }
+}
