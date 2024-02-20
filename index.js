79571b581f6549603a6316532de8214c9dcbb2f7
harshnugget <76251920+harshnugget@users.noreply.github.com>
2024-02-20 19:50:30 +0000

    Improved functionality and bug fix
    index.html:
      - Added id attributes to enhance readability and access on add and subtract buttons
    
    index.js:
      - Appropriately renamed variable names for multiplication and division symbols
      - Improved code consistency with "addSymbol" and "subtractSymbol"
      - Created "handleOperators" function for easier code management
      - Created "activeNumber" variable for clearer assignment of input values
      - Resolved 16-character limit issue by checking "overwriteDisplayValue"

--- index.js
+++ index.js
@@ -22,20 +22,22 @@ let operator;
 let firstNumber;
 let secondNumber;
 
-const multiplySign = document.querySelector("#multiply-button").textContent;
-const divideSign = document.querySelector("#divide-button").textContent;
+const addSymbol = document.querySelector("#add-button").textContent;
+const subtractSymbol = document.querySelector("#subtract-button").textContent;
+const multiplySymbol = document.querySelector("#multiply-button").textContent;
+const divideSymbol = document.querySelector("#divide-button").textContent;
 
 function operate(operator, firstNumber, secondNumber) {
     firstNumber = +firstNumber;
     secondNumber = +secondNumber;
     switch (operator) {
-        case "+":
+        case addSymbol:
             return add(firstNumber, secondNumber);
-        case "-":
+        case subtractSymbol:
             return subtract(firstNumber, secondNumber);
-        case multiplySign:
+        case multiplySymbol:
             return multiply(firstNumber, secondNumber);
-        case divideSign:
+        case divideSymbol:
             // Handle divide by 0
             if (+secondNumber === 0) {
                 return console.log("Cannot divide by 0");
@@ -46,22 +48,29 @@ function operate(operator, firstNumber, secondNumber) {
     }
 }
 
-// Assign variable for display text box
-let display = document.querySelector("#display");
+// Assign variable for display input box
+const display = document.querySelector("#display");
 
 // Add event listeners to each number button and update display with selected number
 document.querySelectorAll(".number").forEach(e => {
     e.addEventListener("click", e => {
-        let number = e.target.textContent;
+        const number = e.target.textContent;
+        
         updateDisplay(number);
     });
 });
 
-// Add event listeners to each operation button and update operator variable
+// Add event listeners to each operator button
 document.querySelectorAll(".operation").forEach(e => {
     e.addEventListener("click", e => {
+        // Assign chosen operator to a variable
+        const currentOperator = e.target.textContent;
+
+        // Set current display value to be overwritten on next input
         overwriteDisplayValue = true;
-        operator = e.target.textContent;
+
+        // Perform checks on chosen operator to calculate next value
+        handleOperators(currentOperator);
     });
 });
 
@@ -81,18 +90,43 @@ display.addEventListener("keydown", e => {
 // Initialize variable for overwriting the display value
 let overwriteDisplayValue = true;
 
+// Initialize variable for toggling between first and second number assignment
+let activeNumber = "firstNumber";
+
 function updateDisplay(number) {
-    // Limit number of characters to 16
-    if (display.value.length >= 16) {
-        return;
+    // Overwrite display with the current number
+    if (overwriteDisplayValue === true) {
+        display.value = number;
+        if (overwriteDisplayValue === true) {
+            overwriteDisplayValue = false;
+        }
+    } else {
+        // Limit number of characters to 16
+        if (display.value.length >= 16) {
+            return;
+        }
+        // Append number to display value
+        display.value += number; 
     }
 
-    // Overwrite display
-    if (display.value === "0" || overwriteDisplayValue === true) {
-        display.value = number;
-        overwriteDisplayValue = false;
-        return;
+    // Switches between assigning value to first number and second number
+    activeNumber === "firstNumber" ? firstNumber = display.value : secondNumber = display.value;
+}
+
+function handleOperators(currentOperator) {
+    // Check if operator is equals sign and necessary values are defined
+    if (currentOperator === "=" && firstNumber !== undefined && operate !== undefined && secondNumber !== undefined) {
+        // Perform calculation and update display
+        firstNumber = operate(operator, firstNumber, secondNumber);
+        display.value = firstNumber;
+        activeNumber = "firstNumber";
+    } else if (currentOperator !== "=") {
+        // Update global operator variable if not equals sign
+        operator = currentOperator;
+        activeNumber = "secondNumber";
+    } else if (firstNumber !== undefined && operate !== undefined && secondNumber !== undefined) {
+        // Perform calculation when firstNumber, operator, and secondNumber are defined
+        firstNumber = operate(operator, firstNumber, secondNumber);
+        display.value = firstNumber;
     }
-    
-    display.value += number;
 }
